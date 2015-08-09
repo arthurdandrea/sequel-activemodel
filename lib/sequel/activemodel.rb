bb20f585266c53df0df799fcf64ca44dc547629e
Arthur D'Andréa Alemar <aalemmar@gmail.com>
2015-08-09 12:08:49 -0300

    `bundle gem sequel-activemodel`

---
+++ lib/sequel/activemodel.rb
@@ -0,0 +1,7 @@
+require "sequel/activemodel/version"
+
+module Sequel
+  module Activemodel
+    # Your code goes here...
+  end
+end
---
+++ lib/sequel/activemodel/version.rb
@@ -0,0 +1,5 @@
+module Sequel
+  module Activemodel
+    VERSION = "0.1.0"
+  end
+end
